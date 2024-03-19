b7cff35eaa2174902ea5923595c9ad232b6eeb37
Jacek <jacekszwarc1@gmail.com>
2024-03-19 12:48:38 +0100

    Naming convention fix

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,6 +5,11 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
+
     path('add_new_financial_status/', views.add_new_financial_status, name='add_new_financial_status'),
-    path('edit_financial_status/<int:financial_status_id>/', views.edit_financial_status, name='edit_financial_status')
+    path('edit_financial_status/<int:financial_status_id>/', views.edit_financial_status, name='edit_financial_status'),
+
+    # TODO
+    # path('add_new_earning_source/', views.add_new_earning_source, name='add_new_earning_source'),
+    # path('add_new_earning/', views.add_new_earning, name='add_new_earning')
 ]
