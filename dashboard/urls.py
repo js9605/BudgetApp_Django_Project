4bbfeba2371edb3a60b45661e5dccd887be6257d
Jacek <jacekszwarc1@gmail.com>
2024-03-19 13:12:38 +0100

    Separate financial status app from dashboard

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,13 +1,13 @@
 from . import views
 from django.urls import path
 
-from . import views
+from financial_status.views import add_new_financial_status, edit_financial_status
 
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
 
-    path('add_new_financial_status/', views.add_new_financial_status, name='add_new_financial_status'),
-    path('edit_financial_status/<int:financial_status_id>/', views.edit_financial_status, name='edit_financial_status'),
+    path('add_new_financial_status/', add_new_financial_status, name='add_new_financial_status'),
+    path('edit_financial_status/<int:financial_status_id>/', edit_financial_status, name='edit_financial_status'),
 
     # TODO
     # path('add_new_earning_source/', views.add_new_earning_source, name='add_new_earning_source'),
