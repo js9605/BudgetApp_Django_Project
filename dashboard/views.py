ae418854dd8490cbe923ff136fae3e57eb088325
Jacek <jacekszwarc1@gmail.com>
2024-08-25 11:28:17 +0200

    Init

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,6 +3,7 @@ from typing import List, Dict, Any
 from django.urls import reverse
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
+from decimal import Decimal
 
 from .models import Dashboard
 from financial_status.forms import FinancialStatusForm
@@ -21,12 +22,9 @@ def dashboard(request):
     earning_source_form = EarningsTrackingForm()
     earning_source_data = user_dashboard.get_earning_sources()
 
-    print("DEBUG: last_financial_status_data", last_financial_status_data)
-    print("DEBUG: earning_source_data", earning_source_data)
-
-    estimate_future_earnings = estimate_earnings(request, earning_source_data)
-    latest_financial_status_amount_float = float(last_financial_status_data[0]['amount'])
-    estimated_account_balance_list = [(float(earning) + latest_financial_status_amount_float) for earning in estimate_future_earnings]
+    financial_status_total_amount = sum_current_financial_status(last_financial_status_data)
+    estimate_future_earnings = estimate_earnings(request, earning_source_data, financial_status_total_amount) #TODO Add expenses wyliczane z sredniej wydatkow co miesiac
+    estimated_account_balance_list = [Decimal(str(earning)) + financial_status_total_amount for earning in estimate_future_earnings]
 
     context = {
         'estimated_account_balance_list': estimated_account_balance_list,
@@ -43,12 +41,19 @@ def dashboard(request):
 def generate_urls(data: List[Dict[str, Any]], url_name):
     return [reverse(url_name, args=[entry['id']]) for entry in data]
 
-def estimate_earnings(request, earning_source_data):
+def estimate_earnings(request, earning_source_data, financial_status_total_amount):
     estimate_earnings_for_future_2_months = []
-    estimated_earnings_list = generate_estimated_earnings_list(request, earning_source_data)
+    estimated_earnings_list = generate_estimated_earnings_list(request, earning_source_data, financial_status_total_amount) #TODO Add expenses wyliczane z sredniej wydatkow co miesiac
 
     current_month_number = datetime.now().month
     estimate_earnings_for_future_2_months.append(estimated_earnings_list[current_month_number - 1])
     estimate_earnings_for_future_2_months.append(estimated_earnings_list[current_month_number])
 
     return estimate_earnings_for_future_2_months
+
+def sum_current_financial_status(last_financial_status_data):
+    financial_status_total_amount = Decimal('0.00')
+    for entry in last_financial_status_data:
+        financial_status_total_amount += entry['amount']
+
+    return financial_status_total_amount
